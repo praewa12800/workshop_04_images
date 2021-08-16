745400893d91b42fc8a087ed6a144ec77e4240ff
praewa12800 <praewa12800@gmail.com>
2021-08-16 10:59:38 +0700

    first commit

---
+++ src/test/java/com/praewa/workshop3_customize/Workshop3Customize1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praewa.workshop3_customize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop3Customize1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
